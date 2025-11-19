bdcba05284d07b4b13c614850877f05827e92325
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-11-19 11:08:19 +0100

    only output .a files for static builds

--- pkgs/by-name/te/tensorflow-lite/package.nix
+++ pkgs/by-name/te/tensorflow-lite/package.nix
@@ -428,12 +428,12 @@ stdenv.mkDerivation rec {
       chmod -x "$path"
     done
 
-    find . -type f -name "*.a" -exec cp {} $out/lib \;
-      
-    ${if !stdenv.hostPlatform.isStatic then ''
+    ${if stdenv.hostPlatform.isStatic then ''
+      find . -type f -name "*.a" -exec cp {} $out/lib \;
+    '' else ''
       # copy libtensorflow-lite.so, libtensorflow-lite.so.2200, libtensorflow-lite.so.2.20.0 etc
       find . -name "*.so*" \( -type f -o -type l \) -exec cp -P {} $out/lib \;
-    '' else ""}
+    ''}
 
     runHook postInstall
   '';
